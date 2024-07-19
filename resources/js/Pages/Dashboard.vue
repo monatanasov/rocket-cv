a54b182b8eafc687e4bbac1fc7c69320d5f00bf1
monatanasov <simeon.g.atanasov@gmail.com>
2024-07-19 18:42:10 +0300

    Add name fields, birthdate date picker and university dropdown

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,13 +1,51 @@
 
 <template>
     <Head title="Dashboard" />
-
     <AuthenticatedLayout>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
-                </div>
+        <div class="flex flex-col max-w-screen-xl mx-auto my-4 border-2 border-red-500">
+            <div class="flex flex-col max-w-sm">
+                <label for="name">Име:</label>
+                <p-input-text
+                    id="name"
+                    v-model="name"
+                    placeholder="Име..."
+                />
+            </div>
+            <div class="flex flex-col max-w-sm">
+                <label for="fathersName">Презиме:</label>
+                <p-input-text
+                    id="fathersName"
+                    v-model="fathersName"
+                    placeholder="Презиме..."
+                />
+            </div>
+            <div class="flex flex-col max-w-sm">
+                <label for="surname">Фамилия:</label>
+                <p-input-text
+                    id="surname"
+                    v-model="surname"
+                    placeholder="Фамилия..."
+                />
+            </div>
+            <div class="flex justify-between max-w-sm">
+                <label for="birthDate">Date of birth:</label>
+                <p-date-picker
+                    v-model="birthDate"
+                    class="flex"
+                    showIcon
+                    fluid
+                    iconDisplay="input"
+                    inputId="birthDate"
+                />
+            </div>
+            <div class="flex justify-between max-w-sm">
+                <p-dropdown
+                    v-model="selectedUniversity"
+                    :options="university"
+                    optionLabel="name"
+                    placeholder="Select a City"
+                    class="w-full md:w-14rem"
+                />
             </div>
         </div>
     </AuthenticatedLayout>
@@ -16,4 +54,19 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import { ref } from 'vue';
+import pInputText from 'primevue/inputtext';
+import pDatePicker from 'primevue/datepicker';
+import pDropdown from 'primevue/dropdown';
+
+
+const name = ref(null);
+const fathersName = ref(null);
+const surname = ref(null);
+const birthDate = ref();
+const selectedUniversity = ref();
+const university = ref([
+    { name: 'University of Economics', code: 'UE' },
+    { name: 'Technical University', code: 'TU' },
+]);
 </script>
